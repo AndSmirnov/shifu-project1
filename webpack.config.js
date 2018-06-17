7de47bfdb3cb3a70f566d8f26a38c444212b390b
ANDREW <smvandrew@gmail.com>
2018-06-18 01:33:14 +0300

    release v1

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  
+  entry: { main: './src/js/main.js' },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, './dist')
+  },
+
+  module: {
+
+    rules: [
+      
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+
+    new MiniCssExtractPlugin({
+        filename: './style.css',
+    }),
+
+    new HtmlWebpackPlugin({
+        filename: 'index.html'
+    })
+  ]
+};
