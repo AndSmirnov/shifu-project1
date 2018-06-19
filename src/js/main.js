94ff4f4be4ceba442ff36230d46be75ebbfa4a8d
ANDREW <smvandrew@gmail.com>
2018-06-19 21:06:33 +0300

    Frontend#2Light#1

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,39 @@ import React from 'react';
 // this lib is needed to place our React App into the HTML
 import ReactDOM from 'react-dom';
 
-// this is the minimal example
-class App extends React.Component {
-  render() {
-    return (<p>Андрей запустил проект, какой он молодец!</p>);
-  }
-}
+import data from './data.json';
 
-// Also it can be written in this way. However this way is going to be taught later.
-// const App = () => <p>Hello world</p>;
+
+class App extends React.Component {
+ 
+
+  render() 
+          {
+      return (
+      
+        <ul>
+        {
+          data.map(function(DataJSON){
+            return <li>{DataJSON.title} - {DataJSON.content} - {DataJSON.slug}</li>;
+          })
+        }
+        </ul>
+      
+      
+      );
+           }
+
+
+};
+  
+export default App;
+
+
 
 // this is responsible write it on the DOM.
 ReactDOM.render(<App />, document.querySelector('#app'));
+
+
+
+
+  
